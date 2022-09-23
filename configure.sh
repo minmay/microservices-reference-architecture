9f7289c513f1a0f50562e72d62b24b070b36dfad
minmay <mvillalobos@kineteque.com>
2022-09-23 13:54:35 -0700

    configure now configures group id and artifact id.

--- configure.sh
+++ configure.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/env bash
 
 buildpackages() {
-  SUFFIX=suffix
-  COMPANY=company
-  PRODUCT=product
-  COMPONENT=component
-  COMMON_NAME=Company
+  SUFFIX_DEFAULT=suffix
+  COMPANY_DEFAULT=company
+  PRODUCT_DEFAULT=product
+  COMPONENT_DEFAULT=component
+  COMMON_NAME_DEFAULT="Company, Inc."
 
   echo "First we are going to derive the root package structure for this java project."
   echo "Remember to use valid package names in the following prompts."
   echo "Please use abbreviations where appropriate because package names are code."
 
-  read -p "Please enter the company's common name. This will be used for the docker company label [$COMMON_NAME]:" COMMON_NAME
-  read -p "Please enter the company domain suffix in lowercase [$SUFFIX]: " SUFFIX
-  read -p "Please enter the company domain name without the suffix in lowercase [$COMPANY]: " COMPANY
-  read -p "Please enter the abbreviated or short product name in lowercase [$PRODUCT]: " PRODUCT
-  read -p "Please enter the abbreviated or short component name in lowercase [$COMPONENT]: " COMPONENT
-
+  read -p "Please enter the company's common name. This will be used for the docker company label [${COMMON_NAME:-$COMMON_NAME_DEFAULT}]: " COMMON_NAME
+  COMMON_NAME=${COMMON_NAME:-$COMMON_NAME_DEFAULT}
+  read -p "Please enter the company domain suffix in lowercase. This will be part of the package name [${SUFFIX:-$SUFFIX_DEFAULT}]:  " SUFFIX
+  SUFFIX=${SUFFIX:-$SUFFIX_DEFAULT}
+  read -p "Please enter the company domain name without the suffix in lowercase. This will be part of the package name [${COMPANY:-$COMPANY_DEFAULT}]:  " COMPANY
+  COMPANY=${COMPANY:-$COMPANY_DEFAULT}
+  read -p "Please enter the abbreviated or short product name in lowercase. This will be part of the package name [${PRODUCT:-$PRODUCT_DEFAULT}]:  " PRODUCT
+  PRODUCT=${PRODUCT:-$PRODUCT_DEFAULT}
+  read -p "Please enter the abbreviated or short component name in lowercase. This will be part of the package name [${COMPONENT:-$COMPONENT_DEFAULT}]:  " COMPONENT
+  COMPONENT=${COMPONENT:-$COMPONENT_DEFAULT}
+  echo ""
+  read -p "Please enter the group-id lower-kebab-case. This will be part of the gradle group [${GROUP_ID:-$SUFFIX.$COMPANY.$PRODUCT}]:  " GROUP_ID
+  GROUP_ID=${GROUP_ID:-$SUFFIX.$COMPANY.$PRODUCT}
+  read -p "Please enter the artifact-id lower-kebab-case. This will be the gradle rootProject.name and jar name and usually has the product name in it [${ARTIFACT_ID:-$COMPONENT}]:  " ARTIFACT_ID
+  ARTIFACT_ID=${ARTIFACT_ID:-$COMPONENT}
+  echo ""
   PACKAGE="$SUFFIX.$COMPANY.$PRODUCT.$COMPONENT"
   read -p "The root package name will be: [$PACKAGE] do you accept [y/n]?: " accept
   case "$accept" in
@@ -47,7 +57,7 @@ renamepackages() {
   git mv src/test/java/suffix/company src/test/java/suffix/$COMPANY
   git mv src/main/java/suffix src/main/java/$SUFFIX
   git mv src/test/java/suffix src/test/java/$SUFFIX
-  git mv src/main/docker-compose/envs/component.env src/main/docker-compose/envs/${COMPONENT}.env
+  git mv src/main/docker-compose/envs/component.env src/main/docker-compose/envs/${ARTIFACT_ID}.env
   echo "pruning empty directories."
   find src -empty -type d -delete
   echo "fixing packages."
@@ -55,10 +65,10 @@ renamepackages() {
   echo "fixing imports."
   find src -name "*.java" -exec sed -i '' "s/import suffix\.company\.product\.component/import $PACKAGE/" {} +
   echo "fixing build.gradle"
-  sed -i '' "s/group = \'suffix\.company\.product\'/group = \'$SUFFIX.$COMPANY.$PRODUCT\'/" build.gradle
-  sed -i '' "s/archiveFileName = \'component.jar\'/archiveFileName = \'$COMPONENT\.jar\'/" build.gradle
+  sed -i '' "s/group = \'suffix\.company\.product\'/group = \'$GROUP_ID\'/" build.gradle
+  sed -i '' "s/archiveFileName = \'component.jar\'/archiveFileName = \'$ARTIFACT_ID\.jar\'/" build.gradle
   echo "fixing settings.gradle"
-  sed -i '' "s/component/$COMPONENT/" settings.gradle
+  sed -i '' "s/component/$ARTIFACT_ID/" settings.gradle
   echo "fixing src/main/resources/application.yaml"
   sed -i '' "s/spring\.application\.name\: component/spring\.application\.name\: $COMPONENT/" src/main/resources/application.yaml
   sed -i '' "s/spring\.datasource\.username: component/spring\.datasource\.username: $COMPONENT/" src/main/resources/application.yaml
@@ -68,21 +78,21 @@ renamepackages() {
   sed -i '' "s/image\: company\.suffix\/product\/component\:latest/image\: $COMPANY\.$SUFFIX\/$PRODUCT\/$COMPONENT\:latest/" docker-compose.yaml
   # \- src\/main\/docker\-compose\/envs\/component\.env
   sed -i '' "s/src\/main\/docker\-compose\/envs\/component\.env/src\/main\/docker\-compose\/envs\/$COMPONENT\.env/" docker-compose.yaml
-  echo "fixing src/main/docker-compose/envs/${COMPONENT}.env"
-  sed -i '' "s/postgresql\.db\.component\.product\.company\.suffix\:5432\/component/postgresql\.db\.$COMPONENT\.$PRODUCT\.$COMPANY\.$SUFFIX\:5432\/$COMPONENT/" src/main/docker-compose/envs/${COMPONENT}.env
+  echo "fixing src/main/docker-compose/envs/${ARTIFACT_ID}.env"
+  sed -i '' "s/postgresql\.db\.component\.product\.company\.suffix\:5432\/component/postgresql\.db\.$COMPONENT\.$PRODUCT\.$COMPANY\.$SUFFIX\:5432\/$COMPONENT/" src/main/docker-compose/envs/${ARTIFACT_ID}.env
   echo "fixing src/main/docker-compose/envs/db.env"
   sed -i '' "s/component/$COMPONENT/" src/main/docker-compose/envs/db.env
   echo "fixing src/main/docker-compose/schemaspy/schemaspy.properties"
   sed -i '' "s/postgresql\.db\.component\.product\.company\.suffix/postgresql\.db\.$COMPONENT\.$PRODUCT\.$COMPANY\.$SUFFIX/" src/main/docker-compose/schemaspy/schemaspy.properties
   sed -i '' "s/component/$COMPONENT/" src/main/docker-compose/schemaspy/schemaspy.properties
   echo "fixing run.sh"
-  sed -i '' "s/component/$COMPONENT/" run.sh
+  sed -i '' "s/component/$ARTIFACT_ID/" run.sh
   echo "fixing Dockerfile"
   sed -i '' "s/company\.suffix\/product\/component/$COMPANY\.$SUFFIX\/$PRODUCT\/$COMPONENT/g" Dockerfile
   sed -i '' "s/LABEL vendor=\"Company\"/LABEL vendor=\"$COMMON_NAME\"/" Dockerfile
-  sed -i '' "s/LABEL app=\"component\"/LABEL app=\"$COMPONENT\"/" Dockerfile
+  sed -i '' "s/LABEL app=\"component\"/LABEL app=\"$ARTIFACT_ID\"/" Dockerfile
   sed -i '' "s/company\/product\/component/$COMPANY\/$PRODUCT\/$COMPONENT/" Dockerfile
-  sed -i '' "s/component\.jar/$COMPONENT\.jar/" Dockerfile
+  sed -i '' "s/component\.jar/$ARTIFACT_ID\.jar/" Dockerfile
 
   # sed -i '' "s/pattern/substitution/" file
 }
@@ -90,5 +100,10 @@ renamepackages() {
 until buildpackages; do : ; done
 
 renamepackages;
+
+
+
+
+
 
 
